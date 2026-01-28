2b87fc188e433e649a4927f3b3db43149d507161
jorgealgar <lester.str@hotmail.com>
2026-01-28 21:15:28 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const hamburger = document.getElementById('hamburger');
+const navMenu = document.getElementById('navMenu');
+
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('active');
+    navMenu.classList.toggle('active');
+});
+
+// Cerrar el menú al hacer clic en un enlace
+document.querySelectorAll('.nav-menu a').forEach(link => {
+    link.addEventListener('click', () => {
+        hamburger.classList.remove('active');
+        navMenu.classList.remove('active');
+    });
+});
